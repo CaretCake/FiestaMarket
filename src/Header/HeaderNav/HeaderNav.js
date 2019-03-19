b90bd2efea8ce4baedde133d310e49ebc77dad82
CaretCake <caretcake@outlook.com>
2019-03-18 23:29:28 -0400

    Nav styling

--- src/Header/HeaderNav/HeaderNav.js
+++ src/Header/HeaderNav/HeaderNav.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import fiestaMarketLogo from './fiesta-market-logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignOutAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 class HeaderNav extends React.Component {
 
@@ -14,10 +16,10 @@ class HeaderNav extends React.Component {
                 <a href='/' class="logo"><img src={fiestaMarketLogo} alt="Logo"/></a>
                 <ul class="nav-list">
                     <li><button onClick={this.displayStatusOptions} class="nav">Status: Online</button></li>
-                    <li><button className="nav">Messages</button></li>
+                    <li><button className="nav"><FontAwesomeIcon icon={faEnvelope}/> Messages</button></li>
                     <li><button className="nav">Notifications</button></li>
                     <li><button className="nav">Account</button></li>
-                    <li><button className="nav">Sign Out</button></li>
+                    <li><button className="nav"><FontAwesomeIcon icon={faSignOutAlt} /> Sign Out</button></li>
                 </ul>
             </div>
         );
