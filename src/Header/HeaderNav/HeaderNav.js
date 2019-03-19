c3f1349554bc55e56bc1aa0c02ad5701c85702d8
CaretCake <caretcake@outlook.com>
2019-03-18 22:57:10 -0400

    Nav logo and styling

--- src/Header/HeaderNav/HeaderNav.js
+++ src/Header/HeaderNav/HeaderNav.js
@@ -1,11 +1,23 @@
-import './HeaderNav.scss';
 import React from 'react';
+import fiestaMarketLogo from './fiesta-market-logo.png';
 
 class HeaderNav extends React.Component {
+
+    displayStatusOptions() {
+        console.log("hello!");
+    }
+
     render() {
         return (
             <div class="header-container">
-                <a href=''><img src=''/></a>
+                <a href='/' class="logo"><img src={fiestaMarketLogo} alt="Logo"/></a>
+                <ul class="nav-list">
+                    <li><button onClick={this.displayStatusOptions} class="nav">Status: Online</button></li>
+                    <li><button className="nav">Messages</button></li>
+                    <li><button className="nav">Notifications</button></li>
+                    <li><button className="nav">Account</button></li>
+                    <li><button className="nav">Sign Out</button></li>
+                </ul>
             </div>
         );
     }
