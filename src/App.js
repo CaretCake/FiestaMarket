f91046fff566215aceec6f27b409b64fd82d3b79
CaretCake <caretcake@outlook.com>
2019-03-18 21:41:14 -0400

    Created header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import HeaderNav from './Header/HeaderNav/HeaderNav'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <HeaderNav />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ src/Header/HeaderNav/HeaderNav.js
@@ -0,0 +1,14 @@
+import './HeaderNav.scss';
+import React from 'react';
+
+class HeaderNav extends React.Component {
+    render() {
+        return (
+            <div class="header-container">
+                <a href=''><img src=''/></a>
+            </div>
+        );
+    }
+}
+
+export default HeaderNav;
