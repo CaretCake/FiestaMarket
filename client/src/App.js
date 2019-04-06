dadeeeda1391db453c391cbfea122775efe17b9a
CaretCake <caretcake@outlook.com>
2019-04-06 18:17:33 -0400

    Start of implementation of basic API

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,20 @@ import HeaderNav from './Header/HeaderNav/HeaderNav'
 import PageFooter from './Footer/PageFooter'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
 
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
 
   render() {
     return (
@@ -17,7 +30,7 @@ class App extends Component {
           </div>
           <div class="container">
             <div class="search">
-
+              <p className="App-intro">{this.state.apiResponse}</p>
             </div>
           </div>
         </div>
