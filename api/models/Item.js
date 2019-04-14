b7cba5fc792550ac44fa1054e084b42a0738c549
CaretCake <caretcake@outlook.com>
2019-04-14 18:20:05 -0400

    Removed top Type enum in item model

--- api/models/Item.js
+++ api/models/Item.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, type) => {
       allowNull: false
     },
     Type: {
-      type: type.ENUM('earrings', 'necklace', 'ring', 'top', 'chest', 'pants', 'boots', 'bracelet', 'mace', 'hammer', '1-hand sword', '2-hand sword', 'axe', 'bow', 'crossbow', 'staff', 'wand', 'dual claws', 'dual swords', 'blade'),
+      type: type.ENUM('earrings', 'necklace', 'ring', 'chest', 'pants', 'boots', 'bracelet', 'mace', 'hammer', '1-hand sword', '2-hand sword', 'axe', 'bow', 'crossbow', 'staff', 'wand', 'dual claws', 'dual swords', 'blade'),
       allowNull: false
     },
     Class: {
