85f9e913188e1a2c9f0420b5bf821d469f3f36c4
CaretCake <caretcake@outlook.com>
2019-04-15 15:15:00 -0400

    Adjusted Item model enums

--- api/models/Item.js
+++ api/models/Item.js
@@ -13,15 +13,15 @@ module.exports = (sequelize, type) => {
       unique: true
     },
     ItemRarity: {
-      type: type.ENUM('white', 'green', 'blue', 'setblue', 'yellow', 'orange', 'purple'),
+      type: type.ENUM('white', 'green', 'blue', 'setblue', 'yellow', 'orange', 'pink'),
       allowNull: false
     },
     Type: {
-      type: type.ENUM('earrings', 'necklace', 'ring', 'head', 'chest', 'pants', 'boots', 'bracelet', 'mace', 'hammer', 'cleric shield', 'fighter shield', '1-hand sword', '2-hand sword', 'axe', 'bow', 'crossbow', 'staff', 'wand', 'dual claws', 'dual swords', 'blade'),
+      type: type.ENUM('earrings', 'necklace', 'ring', 'head', 'chest', 'pants', 'boots', 'bracelet', 'mace', 'hammer', 'shield', '1-hand sword', '2-hand sword', 'axe', 'bow', 'crossbow', 'staff', 'wand', 'dual claws', 'dual swords', 'blade'),
       allowNull: false
     },
     Class: {
-      type: type.ENUM('fighter', 'knight', 'gladiator', 'cleric', 'holy knight', 'guardian', 'mage', 'wizard', 'warlock', 'archer', 'ranger', 'sharpshooter', 'trickster', 'reaper', 'spectre', 'crusader', 'templar', 'all'),
+      type: type.ENUM('fighter', 'knight', 'gladiator', 'cleric', 'holy knight', 'guardian', 'mage', 'wizard', 'warlock', 'archer', 'ranger', 'sharpshooter', 'trickster', 'reaper', 'spectre', 'crusader', 'templar', 'fighter and cleric', 'guardian and holyknight', 'all'),
       allowNull: false
     },
     Level: {
