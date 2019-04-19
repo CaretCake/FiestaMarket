1fb1d804d2a8c2aa49e9e8613e1bd36c433a7032
CaretCake <caretcake@outlook.com>
2019-04-18 23:43:19 -0400

    Added Formik and Yup, added form styling, revamped contact form

--- client/src/scenes/Forms/ContactForm.js
+++ client/src/scenes/Forms/ContactForm.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import "react-tabs/style/react-tabs.css";
+import { Formik, FormikProps, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 export class ContactForm extends React.Component {
 
-  constructor(props) {
-    super(props);
-  }
+  handleSubmit = (values, {
+      props = this.props,
+      setSubmitting
+    }) => {
 
-  handleSubmit = (e) => {
-    e.preventDefault();
+    //process form submission here
+
+
+    //done submitting, set submitting to false
+    setSubmitting(false);
+    return;
   }
 
   handleChange = (e) => {
@@ -16,22 +22,77 @@ export class ContactForm extends React.Component {
   }
 
   render() {
+    const contactSchema = Yup.object().shape({
+      email: Yup.string()
+        .email('Invalid email')
+        .required('*'),
+      message: Yup.string()
+        .min(2, '')
+        .max(250, '250 character limit hit')
+        .required('*')
+    });
+
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Select why you are contacting us:
-          <select onChange={this.handleChange}>
-            <option value="feedback">General Feedback</option>
-            <option value="bug">Report a Bug</option>
-            <option value="question">Ask a Question</option>
-          </select>
-        </label>
-        <label>
-          Message:
-          <textarea onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div class='form-container'>
+        <Formik
+          initialValues={{
+            first_name: '',
+            email: '',
+            gender: ''
+          }}
+          validationSchema={contactSchema}
+          onSubmit={this.handleSubmit}
+          render={formProps => {
+            return(
+              <Form>
+                <div className='field-container'>
+                  <div className='field-label-container'>
+                    <label>Reason for Contact</label>
+                  </div>
+                  <Field
+                    component='select'
+                    name='contactType'
+                    className='selection'
+                    >
+                    <option value='feedback'>General Feedback</option>
+                    <option value='bug'>Report a Bug</option>
+                    <option value='question'>Ask a Question</option>
+                  </Field>
+                </div>
+                <div class='field-container'>
+                  <div className='field-label-container'>
+                    <label>Email Address</label>
+                    <span><ErrorMessage name='email' /></span>
+                  </div>
+                  <Field
+                    type='text'
+                    name='email'
+                    placeholder='Email Address'
+                    className='textbox'
+                    />
+                </div>
+                <div className='field-container'>
+                  <div className='field-label-container'>
+                    <label>Message</label>
+                    <span><ErrorMessage name='message' /></span>
+                  </div>
+                  <Field
+                    component='textarea'
+                    name='message'
+                    placeholder='Type your message here...'
+                    className='textarea'
+                    />
+                </div>
+                <button
+                  type='submit'
+                  disabled={formProps.isSubmitting}>
+                    Submit Form
+                </button>
+              </Form>
+            );
+          }}
+        />
+      </div>
     )
   }
 }
