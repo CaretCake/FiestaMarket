03746e0295a7dc667914877ee14a833dfb8f22ac
CaretCake <caretcake@outlook.com>
2019-04-19 22:39:44 -0400

    Search adjustments and styling for search suggestion dropdown

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './assets/styles/App.scss';
 import { MainApp } from './scenes/export';
 
 
@@ -8,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <MainApp />
+        <MainApp history={this.props.history} />
       </div>
     );
   }
--- client/src/scenes/Main/MainApp.js
+++ client/src/scenes/Main/MainApp.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import axios from 'axios';
 import { itemService } from "../../services/item.service";
 
-export class MainApp extends React.Component {
+export  class MainApp extends React.Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.routeChange = this.routeChange.bind(this);
+
     this.state = {
       query: '',
       results: []
@@ -21,41 +22,42 @@ export class MainApp extends React.Component {
           .then(results => {
             this.setState({results: results.data});
           });
+      } else if (this.state.query.length === 0) {
+        this.setState({results: []});
       }
     });
   }
 
-  getInfo = () => {
-    axios.get(`http://localhost:9000/items/search?&term=${this.state.query}`)
-      .then(({ data }) => {
-        this.setState({
-          results: data
-        })
-      })
-      .catch(err => console.log(err));
+  routeChange(path) {
+    console.log(JSON.stringify(path));
+    this.props.history.push(path);
   }
 
   render() {
-
     return (
-      <div class="main-app-container">
-        <div class="hero-search-section">
-          <div class="flex-left"/>
-          <div class="flex-center">
-            <div class="search-container">
+      <div class='main-app-container'>
+        <div class='hero-search-section'>
+          <div class='flex-left'/>
+          <div class='flex-center'>
+            <div class='search-container'>
               <form>
-                <input placeholder="Search..." name="term" ref={input => this.search = input} onChange={this.handleInputChange} />
-                <button className="search-button">Search</button>
+                <input placeholder='Search...' name='term' ref={input => this.search = input} onChange={this.handleInputChange} autoComplete="off" />
+                <button onClick={() => this.routeChange( '/items/' + (this.state.results[0] !== null ? ((this.state.results[0]).ItemId) : ''))} className='search-button' type="button">Search</button>
               </form>
+              <div className='search-dropdown'>
+                <ul>
+                  {this.state.results.map(item => (
+                    <li key={item.itemId} unselectable="on" class="unselectable">{item.ItemName}</li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
-          <div class="flex-right"/>
+          <div class='flex-right'/>
         </div>
-        <div class="content">
+        <div class='content'>
           <ul>
-            {this.state.results.map(item => (
-              <li key={item.itemId}>{item.ItemName}</li>
-            ))}
+
           </ul>
         </div>
       </div>
