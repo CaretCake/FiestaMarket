f0475dadf29775d0e5f9272f7651a86e6bf04dca
CaretCake <caretcake@outlook.com>
2019-04-25 00:28:10 -0400

    Closes #47; Adjusted user review db model to use isPositive rather than reviewText

--- api/models/UserReview.js
+++ api/models/UserReview.js
@@ -1,16 +1,8 @@
 module.exports = (sequelize, type) => {
   return sequelize.define('UserReviews', {
-    ReviewText: {
-      type: type.STRING(300),
-      allowNull: false,
-      validate: {
-        len: {
-          args: [5, 300],
-          msg: 'Review message must be between 5 and 300 characters'
-        },
-        notNull: true,
-        notEmpty: true
-      }
+    isPositive: {
+      type: type.TINYINT,
+      allowNull: false
     }
   });
 };
